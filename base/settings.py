fd74fa95f025e831fc2c93f7dffcb7e0543ecd15
surajmt8848 <suraj8848mt@gmail.com>
2020-12-05 23:40:22 +0545

    createdocker file

--- base/settings.py
+++ base/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db',
+        'PORT': 3306
+
     }
 }
 
