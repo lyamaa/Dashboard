ef2000eb4cc3d3c4d819378f521581f801cc8ffb
surajmt8848 <suraj8848mt@gmail.com>
2020-12-06 22:38:29 +0545

    create abstractuser

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(models.Model):
+class User(AbstractUser):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 
     def __str__(self):
