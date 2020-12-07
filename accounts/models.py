38776c4877b3cbd1d5ecc5f967b800fa0772bf2c
surajmt8848 <suraj8848mt@gmail.com>
2020-12-07 15:46:55 +0545

    Custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,16 +1,64 @@
-from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
 from django.db import models
 
-class User(AbstractUser):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255, unique=True)
-    password = models.CharField(max_length=255)
-    username = None
+USERNAME_REGEX = '^[a-zA-Z0-9.+-]*$'
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
 
+class MyUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        if not email:
+            raise ValueError('user must have an Email address')
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        user = self.create_user(username, email, password=password)
+        user.is_admin = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    # first_name = models.CharField(max_length=255)
+    # last_name = models.CharField(max_length=255)
+    email = models.EmailField(
+        max_length=255,
+        unique=True,
+        verbose_name='email address')
+
+    # password = models.CharField(max_length=255)
+    username = models.CharField(
+        max_length=255,
+        validators=[
+            RegexValidator(
+                regex=USERNAME_REGEX,
+                message='Username must be alphanumeric or contains numbers',
+                code='Invalid Username'
+            )],
+        unique=True
+    )
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
 
     def __str__(self):
-        return self.first_name
+        return self.email
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,7 +7,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name', 'email', 'password']
+        fields = ['id', 'username', 'email', 'password']
         extra_kwargs = {
             'password': {'write_only': True}
         }
