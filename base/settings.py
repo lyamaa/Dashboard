d301478b832776df8848353c07402c3d2c0d0df2
surajmt8848 <suraj8848mt@gmail.com>
2020-12-07 15:18:55 +0545

    database config

--- base/settings.py
+++ base/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'base.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'root',
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'db',
         'PORT': 5432,
 
