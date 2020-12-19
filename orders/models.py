d766478c59ff779e2d9732af9845d1e8e6a96199
surajmt8848 <suraj8848mt@gmail.com>
2020-12-19 19:59:06 +0545

    created orders model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.db.models.base import Model
 
-# Create your models here.
+class Order(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.email
+    
+
+class OrderItem(models.Model):
+    product_title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product_title
+
