590a09355ac28a2eebced93b74a00aefed11982f
surajmt8848 <suraj8848mt@gmail.com>
2020-12-19 20:00:03 +0545

    create urls, views, serializers

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import ProductGenericAPIView, FileUploadView
+from .views import OrderGenericAPIView
 
 app_name = 'orders'
 
 urlpatterns = [
-   
+   path('orders', OrderGenericAPIView.as_view()),
+   path('orders/<str:pk>', OrderGenericAPIView.as_view()),
 ] 
 
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import generics, mixins
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from accounts.authentication import JWTauthentication
+from .models import Order, OrderItem
+from .serializers import OrderSerializer, OrderItemSerializer
+from base.pagination import CustomPagination
+
+
+class OrderGenericAPIView(
+    generics.GenericAPIView, 
+    mixins.ListModelMixin, 
+    mixins.RetrieveModelMixin
+    ):
+    authentication_classes = [JWTauthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    pagination_class = CustomPagination
+
+    def get(self, request, pk=None):
+        if pk:
+            return Response({
+                'data': self.retrieve(request, pk).data
+            })
+        return self.list(request)
