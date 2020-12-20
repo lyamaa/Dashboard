10ff0a5fb528ba5961fde438a1a62a1361da603f
surajmt8848 <suraj8848mt@gmail.com>
2020-12-20 16:59:18 +0545

    create permission

---
+++ accounts/permissions.py
@@ -0,0 +1,18 @@
+
+from rest_framework import permissions
+from rest_framework.permissions import BasePermission
+from .serializers import UserSerializer
+
+
+class PermissionsView(BasePermission):
+    def has_permission(self, request, view):
+        data = UserSerializer(request.user).data
+
+        view_acess = any(
+            p['name'] == 'view_' + view.permission_object for p in data['role']['permissions'])
+        edit_access = any(
+            p['name'] == 'edit_' + view.permission_object for p in data['role']['permissions'])
+        
+        if request.method == 'GET':
+            return view_acess or edit_access
+        return edit_access
