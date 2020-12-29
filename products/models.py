b8fc595a1480d0f35c389bee6837c6f1e30f44a2
surajmt8848 <suraj8848mt@gmail.com>
2020-12-29 15:48:45 +0545

    update models

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(max_length=1000)
-    image = models.CharField(max_length=255)
+    image = models.CharField(max_length=255, blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
