b2c187c46953f44fb84846e27f028f25a946fa3f
surajmt8848 <suraj8848mt@gmail.com>
2021-01-06 17:53:45 +0545

    add path for template

--- base/urls.py
+++ base/urls.py
@@ -16,14 +16,18 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("accounts.urls", namespace="accounts")),
     path("api/", include('products.urls', namespace='products')),
     path("api/", include('orders.urls', namespace='orders')),
-]
+    path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset'))
+]  
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name="index.html"))]
 if settings.DEBUG:
     # urlpatterns += static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+   
