1f5504292e452ba7f69295199ba186868f2b4862
surajmt8848 <suraj8848mt@gmail.com>
2021-01-06 17:53:20 +0545

    update models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
+from django.dispatch import receiver
+from django.urls import reverse
+from django_rest_passwordreset.signals import reset_password_token_created
+from django.core.mail import send_mail  
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -77,3 +81,20 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+@receiver(reset_password_token_created)
+def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+    
+    email_plaintext_message = "{}?token={}".format(reverse('password_reset:reset-password-request'), reset_password_token.key)
+
+    send_mail(
+        # title:
+        "Password Reset for {title}".format(title="Some website title"),
+        # message:
+        email_plaintext_message,
+        # from:
+        "noreply@somehost.local",
+        # to:
+        [reset_password_token.user.email]
+    )
