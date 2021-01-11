79314f40d17eef5b0eca924fe70939abd7ee570f
surajmt8848 <suraj8848mt@gmail.com>
2021-01-11 11:36:32 +0545

    vue js build file

---
+++ dist/precache-manifest.1e3fd45afbbab162ada445fdcd2d5e93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "762e1a3c9472647a1365",
+    "url": "/css/app.68b2501a.css"
+  },
+  {
+    "revision": "34523f3d34f98faf074928d41e53b01a",
+    "url": "/img/profile.34523f3d.svg"
+  },
+  {
+    "revision": "bb03c6b6ad837295fe09d470d0378894",
+    "url": "/index.html"
+  },
+  {
+    "revision": "762e1a3c9472647a1365",
+    "url": "/js/app.5cdfc17d.js"
+  },
+  {
+    "revision": "18182df254cfbf0ab748",
+    "url": "/js/chunk-vendors.20fd92f9.js"
+  },
+  {
+    "revision": "cc54087102bd1d8d805c8b904d6c3395",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
