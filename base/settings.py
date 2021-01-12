59176d46d021e8d1b39a8056f97434e6ac872bf4
surajmt8848 <suraj8848mt@gmail.com>
2021-01-12 12:48:56 +0545

    modified

--- base/settings.py
+++ base/settings.py
@@ -31,15 +31,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-
     # 3rd party app
     "rest_framework",
     'corsheaders',
     'django_rest_passwordreset',
     'cloudinary',
     'webpack_loader',
-    
-
 
     # MY APP
     "accounts",
