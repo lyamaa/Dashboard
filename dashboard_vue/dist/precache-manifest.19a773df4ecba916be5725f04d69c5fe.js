0d17d3887556017be23543f2f9db8db9f6aea502
surajmt8848 <suraj8848mt@gmail.com>
2021-01-12 13:36:53 +0545

    add dist

---
+++ dashboard_vue/dist/precache-manifest.19a773df4ecba916be5725f04d69c5fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e987d89a9ab95db5855647db7a920168",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc54087102bd1d8d805c8b904d6c3395",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e428b8543cb041c5157f",
+    "url": "/static/css/app.68b2501a.css"
+  },
+  {
+    "revision": "34523f3d34f98faf074928d41e53b01a",
+    "url": "/static/img/profile.34523f3d.svg"
+  },
+  {
+    "revision": "e428b8543cb041c5157f",
+    "url": "/static/js/app.7e9baee9.js"
+  },
+  {
+    "revision": "18182df254cfbf0ab748",
+    "url": "/static/js/chunk-vendors.20fd92f9.js"
+  }
+]);
